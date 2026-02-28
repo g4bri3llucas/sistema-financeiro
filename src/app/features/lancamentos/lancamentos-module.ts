3de61fdc7dad92245b4bc2d244db5aa7387e158c
g4bri3llucas <gabriellucasferreira01.s2@gmail.com>
2026-02-28 10:52:13 -0300

    feat: estrutura de modulos shared e features criada com pipe inicial

---
+++ src/app/features/lancamentos/lancamentos-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LancamentosRoutingModule } from './lancamentos-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, LancamentosRoutingModule],
+})
+export class LancamentosModule {}
---
+++ src/app/shared/pipes/status-financeiro-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StatusFinanceiroPipe } from './status-financeiro-pipe';
+
+describe('StatusFinanceiroPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StatusFinanceiroPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
